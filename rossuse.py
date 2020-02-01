ee7bc8c868cf7a501d77cf6d4a2146d35abde07b
Neotinker <neotinker@gmail.com>
2020-02-01 16:09:52 -0600

    Major update. Many changes. Still not functional.

--- rossuse.py
+++ rossuse.py
@@ -1,11 +1,16 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
-import yaml, em, argparse, re, textwrap, datetime, sys
+import os, yaml, em, argparse, re, textwrap, datetime, sys
 from dateutil import tz
 from rosdistro import get_distribution, get_index, get_index_url, _get_dist_file_data
 from catkin_pkg.package import parse_package_string
 from rosdep2 import create_default_installer_context
 from rosdep2.catkin_support import get_catkin_view
+
+import osc
+import osc.core
+import osc.conf
+
 
 # We will assume that we can generate a spec file good for multiple OS/Versions
 # based on the output from a single OS/version (opensuse 15.1)
@@ -28,10 +33,11 @@ def init_environment():
   rview = get_catkin_view(rdistro,os_name, os_version, False)
 
 def generate_package_list():
-  global os_name, os_version, rdistro, ctx, os_installers, default_os_installer, dist_data, rindex, rcache, rview
+  global args, os_name, os_version, rdistro, ctx, os_installers, default_os_installer, dist_data, rindex, rcache, rview
 
   tlist = []
-  for tpkg in dist_data[0]['repositories'].keys():
+
+  for tpkg in rcache._distribution_file.release_packages.keys():
     tlist.append(tpkg)
 
   return tlist
@@ -116,10 +122,12 @@ def get_pkg_data(pkg_name):
   pkg_data['release']['name'] = rcache._distribution_file.release_packages[pkg_name].name
   pkg_data['release']['repo_name'] = rcache._distribution_file.release_packages[pkg_name].repository_name
   pkg_data['release']['repo_data'] = dist_data[0]['repositories'][pkg_data['release']['repo_name']]['release']
-  pkg_data['release']['src_data'] = dist_data[0]['repositories'][pkg_data['release']['repo_name']]['source']
 
   tmp_catkin_pkg_info = get_package_info(pkg_name)
-  pkg_data['catkin_pkg']['homepage'] = tmp_catkin_pkg_info['urls'][0].url
+  if 'urls' in tmp_catkin_pkg_info and len(tmp_catkin_pkg_info['urls']) > 0:
+    pkg_data['catkin_pkg']['homepage'] = tmp_catkin_pkg_info['urls'][0].url
+  else:
+    pkg_data['catkin_pkg']['homepage'] = []
   pkg_data['catkin_pkg']['version'] = tmp_catkin_pkg_info['version']
   pkg_data['catkin_pkg']['licenses'] = tmp_catkin_pkg_info['licenses']
   pkg_data['catkin_pkg']['description'] = tmp_catkin_pkg_info['description']
@@ -202,14 +210,14 @@ def collect_template_data(pkg_data):
 def generate__service_file(g):
   global os_name, os_version, rdistro, ctx, os_installers, default_os_installer, dist_data, rindex, rcache, rview
 
-  interpreter = em.Interpreter(output=open('_service', "w"))
+  interpreter = em.Interpreter(output=open(g['osc_project'] + '/' + g['osc_package'] + '/_service', "w"))
   interpreter.include('template._service.em',g)
   interpreter.shutdown()
 
 def generate_spec_file(g):
   global os_name, os_version, rdistro, ctx, os_installers, default_os_installer, dist_data, rindex, rcache, rview
 
-  interpreter = em.Interpreter(output=open(g['Name'] + '.spec', "w"))
+  interpreter = em.Interpreter(output=open(g['osc_project'] + '/' + g['osc_package'] + '/' + g['Name'] + '.spec', "w"))
   interpreter.include('template.spec.em',g)
   interpreter.shutdown()
 
@@ -217,10 +225,14 @@ if __name__ == '__main__':
 
   parser = argparse.ArgumentParser(description='Generate an files for building rpms')
   parser.add_argument('--template', help='Define a templte to use for generating spec file')
-  parser.add_argument('--rosdistro', help='ROS Distribution to generate files for')
-  parser.add_argument('--os_name', help='The OS to generate files for')
-  parser.add_argument('--os_version', help='The OS version to generate files for')
-  parser.add_argument('--pkg_name', help='The package to generate files for')
+  parser.add_argument('--osc_project', required=True, help='OSC project where we will be pushing generated files. You need to create this through OSC prior to use.')
+  parser.add_argument('--rosdistro', required=True, help='ROS Distribution to generate files for')
+  parser.add_argument('--os_name', required=True, help='The OS to generate files for')
+  parser.add_argument('--os_version', required=True, help='The OS version to generate files for')
+  parser.add_argument('--generate_all', action='store_true', default=False, required=False, help='Generate files for all packages. By default only generate files for packages not osc project')
+  parser.add_argument('--dry_run', action='store_true', default=False, required=False, help='Dont generate any files or commit them to osc')
+  parser.add_argument('--pkg_name', action='append', help='The package to generate files for')
+  parser.add_argument('--meta', help='Generate files for Metapackage(s) instead of \'regular\' packages', action="store_true", default=False, required=False) 
 
   args = parser.parse_args()
 
@@ -250,24 +262,82 @@ if __name__ == '__main__':
   # Get data from rosdep
   init_environment()
 
+  # Initialize OSC
+  osc.conf.get_config()
+  apiurl = osc.conf.config['apiurl']
+  project = args.osc_project
+  if osc.core.is_project_dir(project):
+    # Project has already been checked out
+    prj = osc.core.Project(project)
+  else:
+    # Project directory does not exist yet
+    prj = osc.core.Project.init_project(apiurl,project,project,osc.conf.config['do_package_tracking'])
+
+  # A package can be a "metapackage" and/or a "regular" package
+  # We build rpms for "regular" packages
+  # "metapackages" are patterns (not sure about building them yet)
+  # Sometimes a name can be shared by both a meta and regular package
+
   # Generate list of packages 
   if pkg_name == None:
-    # Get all
+    # Get a list of all packages
+    # This currently includes Meta and Regular packages
     pkg_list = generate_package_list()
   else:
-    pkg_list = [pkg_name]
+    pkg_list = pkg_name
 
-  for p in pkg_list:
-    pkg_data = get_pkg_data(p)
-    template_data = collect_template_data(pkg_data)
+  ptot = len(pkg_list)
+  pcounter = 0
+  scounter = 0
+  fcounter = 0
+  for p in sorted(pkg_list):
 
-    # Create a working directory for a package
+    pcounter += 1
+    print("{}/{} Looking for package data for {}".format(pcounter,ptot,p))
+
+    try:
+      pkg_data = get_pkg_data(p)
+      template_data = collect_template_data(pkg_data)
+      template_data['osc_project'] = project
+      template_data['osc_package'] = p
+
+      print("Found package data for {}".format(p))
+      if p in osc.core.meta_get_packagelist(apiurl,project) and not args.generate_all:
+        print("{} exists. Skipping".format(p))
+        continue
+      elif p in osc.core.meta_get_packagelist(apiurl,project):
+        # Checkout package info
+        if not args.dry_run:
+          osc.core.checkout_package(apiurl,project,p,prj_dir=project)
+      else:
+        # Doesn't exist in project so initialize a new package
+        print("We should create a new package for {} but I don't know how yet. so skip".format(p))
+        osc.core.edit_meta(metatype='pkg'
+    except Exception:
+      e = sys.exc_info()
+      print("We had problems with {}".format(p))
+      print(e)
+      fcounter += 1
+      continue
+
+    scounter += 1
 
     # Pull data to directory using oSC (if it exists)
+    # This should create a directory.
+    # If it doesn't then we will need to do it manually
 
-    #print template_data
-    generate_spec_file(template_data)
-    generate__service_file(template_data)
+    print("Generating files for {}".format(p))
+    if not args.dry_run:
+      generate_spec_file(template_data)
+      generate__service_file(template_data)
 
     # checkin generated files using osc
+
+    # Flush stdout
+    sys.stdout.flush()
+
+  # Print breakdown of results
+  print("packages (total):{}".format(pcounter))
+  print("packages (success):{}".format(scounter))
+  print("packages (failure):{}".format(fcounter))
 
